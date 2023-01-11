f618bef059723c8d0282e7ff9589c0667d55ef3a
sunwei <sunwei.pyw@gmail.com>
2023-01-11 08:30:17 +0800

    Netty demo1

---
+++ src/main/java/cn/sunway/netty/NettyServer.java
@@ -0,0 +1,44 @@
+package cn.sunway.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author sunw
+ * @date 2023/1/11
+ */
+public class NettyServer {
+    public static void main(String[] args) throws Exception{
+        //创建两个线程组，bossGroup和workerGroup
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup);
+            bootstrap.channel(NioServerSocketChannel.class);
+            bootstrap.option(ChannelOption.SO_BACKLOG, 128);
+            bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new MyServerHandler());
+                }
+            });
+            System.out.println("服务器端已准备就绪...");
+            ChannelFuture channelFuture = bootstrap.bind(6666);
+            channelFuture = channelFuture.sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
